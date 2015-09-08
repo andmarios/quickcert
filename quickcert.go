6e27611266876ac88b2becafd269e42ad4f5d5b9
Marios Andreopoulos <opensource@andmarios.com>
2015-09-09 02:14:02 +0300

    add copyright information

--- quickcert.go
+++ quickcert.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2015, Marios Andreopoulos. All rights reserved.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file that should come with this code.
+
 package main
 
 import (
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package main
 
-const version = "41.705c4de+"
+const version = "47.0fe123a+"
